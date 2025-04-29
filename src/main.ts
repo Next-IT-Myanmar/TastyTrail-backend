2d19737ca510b4238ca160c8f62128a5da1f011e
kyawkyawkhaing@abank.com.mm <kyawkyawkhaing@abank.com.mm>
2025-04-29 16:45:49 +0630

    fixed cros

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.FRONTEND_URL || '*',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   });
 
   app.useGlobalPipes(
